fac8212a5c8dbb8c8c6e0d85cf32858b95930759
NIKATONI <102518913+NIKATONI@users.noreply.github.com>
2023-12-19 13:38:21 +0400

    FirstCommit

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+import Button from '@/components/button/Button'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      
+    </div>
+  )
+}
--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import styles from './navBar.module.css'
 import Link from "next/link"
-import DarkModeToggle from "@/components/darkModeToggle/DarkModeToggle"
 
 export default function Navbar() {
 
@@ -13,28 +12,18 @@ export default function Navbar() {
     },
     {
       id: 2,
-      title: "Portfolio",
+      title: "Shop",
       url: "/portFolio"
     },
     {
       id: 3,
-      title: "Blog",
-      url: "/blog"
-    },
-    {
-      id: 4,
       title: "About",
       url: "/about"
     },
     {
-      id: 5,
+      id: 4,
       title: "Contact",
       url: "/contact"
-    },
-    {
-      id: 6,
-      title: "DashBoard",
-      url: "/dashBoard"
     }
   ]
   
@@ -44,11 +33,9 @@ export default function Navbar() {
         Lamamia
       </Link>
       <div className={styles.links}>
-        <DarkModeToggle/>
         {links.map((link) => (
           <Link href={link.url} className={styles.links}>{link.title}</Link>
         ))}
-        <button className={styles.logout}>Logout</button>
       </div>
     </div>
   );
