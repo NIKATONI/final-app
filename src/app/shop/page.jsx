6724085c6c82b2138b324b790a15692ccaaeeb40
NIKATONI <102518913+NIKATONI@users.noreply.github.com>
2023-12-25 23:40:00 +0400

    AddedFilters

--- src/app/shop/page.jsx
+++ src/app/shop/page.jsx
@@ -4,26 +4,23 @@ import React, { useEffect, useState } from 'react'
 import styles from "./page.module.css"
 import Image from 'next/image';
 
-// const getData = async () => {
-//     const res = await fetch("https://api.escuelajs.co/api/v1/products", { cache: "force-cache" });
-
-//     if (!res.ok) {
-//         throw new Error("There is no data");
-//     }
-
-//     return res.json();
-// }
-
 export default function Shop() {
 
     const [data, setData] = useState([]);
     const [search, setSearch] = useState("");
+    const [fromPrice, setFromPrice] = useState(0);
+    const [toPrice, setToPrice] = useState(0);
 
     useEffect(() => {
         fetch("https://api.escuelajs.co/api/v1/products", { cache: "force-cache" })
             .then((response) => response.json())
             .then((json) => {
                 setData(json);
+                let maxPrice = json.reduce(function (maxPrice, currentPrice) {
+                    return (currentPrice.price > maxPrice.price) ? currentPrice : maxPrice;
+                }, json[0]);
+
+                setToPrice(maxPrice.price);
             });
     }, [])
 
@@ -34,12 +31,29 @@ export default function Shop() {
                 name="searchBar"
                 id="searchBar"
                 placeholder={"What are you looking for...?"}
-                className={styles.searchBar}
+                className={`${styles.searchBar} ${styles.inputF}`}
                 onChange={(e) => { setSearch(e.target.value) }} />
+            <div>
+                <input
+                    type="text"
+                    name="fromPrice"
+                    id="fromPrice"
+                    placeholder={"From"}
+                    className={`${styles.fromPrice} ${styles.inputF}`}
+                    onChange={(e) => { setFromPrice(e.target.value) }} />
+                <input
+                    type="text"
+                    name="toPrice"
+                    id="toPrice"
+                    placeholder={"To"}
+                    value={toPrice}
+                    className={`${styles.toPrice} ${styles.inputF}`}
+                    onChange={(e) => { setToPrice(e.target.value) }} />
+            </div>
             <div className={styles.itemHolder}>
                 {data.filter((i) => {
                     return search.toLowerCase() === "" ?
-                        i : i.title.toLowerCase().includes(search);
+                        i : i.title.toLowerCase().includes(search), i.price > fromPrice && i.price < toPrice;
                 }).map((item) => (
                     <Link href={`/shop/${item.id}`} className={styles.card}>
                         <div className={styles.imageContainer}>
